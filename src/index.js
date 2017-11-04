96fabcf2623bccc8cab5f9ca867fb7ed6204ce10
Webb Peng <webb.png@gmail.com>
2017-11-05 01:13:09 +0800

    remove empty file if path exists

--- src/index.js
+++ src/index.js
@@ -1,37 +1,51 @@
 
 import net from 'net';
-import { remove } from 'fs-extra';
+import { stat } from 'fs';
+import rimraf from 'rimraf';
+import callMaybe from 'call-me-maybe';
 
 export default function createLocalDomainSocket(server, path, callback) {
-	if (process.platform === 'win32') {
-		path = path.replace(/^\//, '').replace(/\//g, '-');
-		path = `\\\\.\\pipe\\${path}`;
-	}
-
-	server.on('error', function handleServerError(err) {
-		if (err.code === 'EADDRINUSE') {
-			const clientSocket = new net.Socket();
-			clientSocket.on('error', function handleClientError(err) {
-				if (err.code === 'ECONNREFUSED') {
-					remove(path, function handleRemoveError(err) {
-						if (err) { callback(err); }
-						else { server.listen(path, callback); }
-					});
-				}
-			});
-
-			clientSocket.connect({ path }, function clientConnect() {
-				const error = new Error('listen EADDRINUSE');
-				Object.assign(error, {
-					errno: 'EADDRINUSE',
-					code: 'EADDRINUSE',
-					syscall: 'listen',
-				});
-				clientSocket.destroy();
-				callback(error);
-			});
+	return callMaybe(callback, new Promise((resolve, reject) => {
+		if (process.platform === 'win32') {
+			path = path.replace(/^\//, '').replace(/\//g, '-');
+			path = `\\\\.\\pipe\\${path}`;
 		}
-	});
 
-	server.listen(path, callback);
+		server.on('error', function handleServerError(err) {
+			if (err.code === 'EADDRINUSE') {
+				const clientSocket = new net.Socket();
+				clientSocket.on('error', function handleClientError(err) {
+					if (err.code === 'ECONNREFUSED' || err.code === 'ENOTSOCK') {
+						stat(path, (e, stats) => {
+							if (!e && stats && !stats.size) {
+								rimraf(path, function handleRemoveError(err) {
+									if (err) { reject(err); }
+									else { server.listen(path, resolve); }
+								});
+							}
+							else {
+								reject(err);
+							}
+						});
+					}
+					else {
+						reject(err);
+					}
+				});
+
+				clientSocket.connect({ path }, function clientConnect() {
+					const error = new Error('listen EADDRINUSE');
+					Object.assign(error, {
+						errno: 'EADDRINUSE',
+						code: 'EADDRINUSE',
+						syscall: 'listen',
+					});
+					clientSocket.destroy();
+					reject(error);
+				});
+			}
+		});
+
+		server.listen(path, resolve);
+	}));
 }
