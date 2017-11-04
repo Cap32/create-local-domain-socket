ef5516e25e010de680eb30209fc061824a883b80
Webb Peng <webb.png@gmail.com>
2017-11-04 23:51:06 +0800

    init module

--- src/index.js
+++ src/index.js
@@ -1,2 +1,31 @@
 
-export default 'it works';
+import net from 'net';
+import { remove } from 'fs-extra';
+
+export default function createLocalDomainSocket(server, path, callback) {
+	server.on('error', function handleServerError(err) {
+		if (err.code === 'EADDRINUSE') {
+			var clientSocket = new net.Socket();
+			clientSocket.on('error', function handleClientError(err) {
+				if (err.code === 'ECONNREFUSED') {
+					remove(path, function handleRemoveError(err) {
+						if (err) { callback(err); }
+						else { server.listen(path, callback); }
+					});
+				}
+			});
+
+			clientSocket.connect({ path }, function clientConnect() {
+				const error = new Error('listen EADDRINUSE');
+				Object.assign(error, {
+					errno: 'EADDRINUSE',
+					code: 'EADDRINUSE',
+					syscall: 'listen',
+				});
+				callback(error);
+			});
+		}
+	});
+
+	server.listen(path, callback);
+}
